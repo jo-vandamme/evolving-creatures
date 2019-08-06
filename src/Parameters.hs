e40c80523bb1bcabc021b709fa20f4e034dde28b
Jonathan Vandamme <jovndmm@gmail.com>
2019-08-06 17:31:25 -0400

    Modified parameters

--- src/Parameters.hs
+++ src/Parameters.hs
@@ -1,19 +1,17 @@
 module Parameters where
 
-simDuration, popSize, numFood, width, height, numSteps, tournamentSize :: Num a => a
+simDuration, popSize, numFood, width, height, tournamentSize :: Num a => a
 vMax, aMax, wMax, eatRadius, crossover, mutation, elitism :: Floating a => a
 width = 600 -- m
 height = 600 -- m
-vMax = 100 -- m/s
-aMax = 20 -- m/s^2
-wMax = 360 * 3 -- deg
+vMax = 200 -- m/s
+aMax = 100 -- m/s^2
+wMax = 360 * 10 -- deg
 popSize = 200
 numFood = 150
 eatRadius = 5 -- m
-numSteps = 1000
 simDuration = 10 -- s
 crossover = 0.8 -- 0-1
 mutation = 0.03 -- 0-1
 elitism = 0.1 -- 0-1
 tournamentSize = 10
-
